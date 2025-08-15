7f1bd035a4390a7bcc0a0b2290b4c8546939cf9b
nugenjs <jn183116@gmail.com>
2025-08-15 15:36:58 -0500

    feat(neet): completed 12

--- NEET/12_intToRoman.ts
+++ NEET/12_intToRoman.ts
@@ -1,5 +1,7 @@
 // I 1, V 5, X 10, L 50, C 100, D 500, M 1000
-// not start with 4 or 9, 
+// not start with 4 or 9, normal
+// start with 4 or 9, use subtractive (e.g. 40 = XL, 900 = CM)
+// only powers of 10 can be consecutive, maximum of 3 times.
 //  
 // 3749: MMMDCCXLIX
 // 58: LVIII
@@ -24,17 +26,26 @@ const intToRoman = (num: number) => {
         const romanMapChar = intToRomanMap[i]?.romanChar!;
         if (num > romanMapInt) {
             const firstDigit = num.toString()[0];
-            if (firstDigit === '4' || firstDigit === '9') {
-                console.log('HEREG OT');
+            let remainder = 0;
+            if (firstDigit === '9') {
+                // Use 10xxx - 1xxx to get 9xxx
+                let {int: tensMapInt, romanChar: tensMapChar} = intToRomanMap[i-1]!;
+                let {int: onesMapInt, romanChar: onesMapChar} = intToRomanMap[i+1]!;
+                remainder = num - (tensMapInt - onesMapInt);
+                romanRes += `${onesMapChar}${tensMapChar}`
             }
-
-            let div = ~~(num / romanMapInt);
-            let remainder = num % romanMapInt;
-
-
-            console.log(`romanMapInt: ${romanMapInt}, romanMapChar: ${romanMapChar},       div ${div}, remainder ${remainder}`)
+            else if (firstDigit === '4') {
+                // Use 5xxx - 1xxx to get 4xxx
+                let {int: fivesMapInt, romanChar: fivesMapChar} = intToRomanMap[i-1]!;
+                remainder = num - (fivesMapInt - romanMapInt);
+                romanRes += `${romanMapChar}${fivesMapChar}`
+            } else {
+                let div = ~~(num / romanMapInt);
+                remainder = num % romanMapInt;
+            
+                romanRes += `${romanMapChar.repeat(div)}`;
+            }
             num = remainder;
-            romanRes += `${romanMapChar.repeat(div)}`;
         }
     }
 
